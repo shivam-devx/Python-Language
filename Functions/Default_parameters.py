add4ca9cf22c8952d76d196a746a2b76a1571a6e
Shivam-devx <shivamdevx369@gmail.com>
2026-02-25 18:56:31 +0530

    Add a folder called function and its some file

---
+++ Functions/Default_parameters.py
@@ -0,0 +1,31 @@
+# a function is a block of code that perform specific task whenever its called 
+# in bigger programs , where we have large amounts of code, it is advisable to create or use existing functions that
+# make the program flow organized and neat
+# There are two types of functions 1. built in functions and 2. user defined functions
+
+# 1. A built in functuons are defined and pre coded in python and some example of built in functions are 
+""" 
+    min(), max(), len(), sum(), type(), range(), dict(), list(), tuple(), set(), print() and etc
+"""
+#2. user defined finctions to create for perform specific tasks as per our needs, such functions are called user defined functions
+# Syntax:
+
+# def function_name(parameter_name and type):
+#     code and statements
+
+# There is def is a keyword and function name is its name in round brackets are parametes then all code and statements perform here
+
+# How function call:
+
+def name(fname, lname):
+    print("Hello,", fname, lname)
+
+name("Shivam","Dohare") # that is function call with parameter and there some type are there
+
+# its called functions arguments 
+# 1. default arguments
+# 2. keyword arguments
+# 3. requird arguments
+# 4. variable length arguments
+
+

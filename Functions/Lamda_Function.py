3960ce5c453724cc8bcfac55fbb9c81b38e5a138
Shivam-devx <shivamdevx369@gmail.com>
2026-03-01 13:23:05 +0530

    complete the function

---
+++ Functions/Lamda_Function.py
@@ -0,0 +1,7 @@
+# A lambda function is a small anonymous function defined using the lambda keyword
+
+# Syntax:
+
+# lambda argruments: expression
+
+# it can take any number of arguments but only one expression

73dc851fb40523bc5c4fe95313ef04db7cfdfaa6
Shivam-devx <shivamdevx369@gmail.com>
2026-03-02 12:17:51 +0530

    Adding info in file handling

--- String_and_File_handling/File_in&out.py
+++ String_and_File_handling/File_in&out.py
@@ -28,6 +28,21 @@ binary(b): used to handle binary files(images)
 
 """
 
+
+# WRITING FILES := writing replaces or appends content
+# Example:
+
+f = open("sample.txt", "w")
+f.write("Hello, Shivam!")
+f.close()
+
+f = open("sample.txt", "a")
+f.write("\nThis is appended text.")
+f.close()
+
+with open("sample.txt", "w") as f:
+    f.write("Using with-statement for safe file handling.")
+    
 # Reading files := you can read entire file, one line, or all lines
 # Example: 
 
@@ -40,11 +55,44 @@ f = open("sample.txt", "r")
 print(f.readline())  # read one line
 f.close()
 
-f = open("samle.txt", "r")
+f = open("sample.txt", "r")
 for line in f:
     print(line.strip())   # read line by line
 
 f.close()
 
+# FILE OPERATIONS := you can check existence, delete, or manipulate files
+# Example:
+import os 
+print(os.path.exists("sample.txt"))  # true and false
+os.remove("sample.txt")     # delete file 
 
-# 
+with open("data.txt", "w") as f:
+    f.write("Line1\nLine2\nLine3")
+    
+with open("data.txt", "r") as f:
+    lines = f.readlines()
+    print(lines)
+    
+# Perfect example of file handling 
+# Step 1: creating a file is primarily done using the create(x)mode
+file = open("Test.txt", "x")
+
+# Step 2: write this method write content onto a file 
+file = open("Text.txt", "w")
+file.write("This is an example of file creation method to writing in file.")
+file.close()
+# overwrite in existing file 
+file = open("Text.txt", "w")
+file.write("This is overwrite text written in file")
+file.close()
+
+# Step 3: Read a file, this method allows us to read the contents of the file
+file = open("Text2.txt", "r")
+print(file.read())
+file.close()
+
+# Step 4: this method appends content into a file
+file = open("nexText.txt", "a")
+file.write("This is an example of file appending")
+file.close()
